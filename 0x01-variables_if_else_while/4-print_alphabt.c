cd885a73cabe88768ff501b2292d4bcae6763948
KgomotsoNeo <motzo.khunou@rocketmail.com>
2023-02-20 19:16:37 -0800

    Print alphabets in lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints alphabet in lower case
+ * followed by new line, except q and e
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+	return (0);
+}
